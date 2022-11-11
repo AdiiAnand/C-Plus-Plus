cf3f6af9680ae2bfe89d912aa93abee636f2af34
Aditya Anand <adityaanand430@gmail.com>
2022-11-11 08:44:48 +0530

    third commit

---
+++ Noob.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+ int main()
+ {
+using namespace std;
+cout<< "I am Noob";
+   }
+
+
+ 
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,7 @@
-#include<iostream>
+#include <iostream>
+
+int main() {
+    int sum=6;
+    std::cout << "I am a Noob!"<<"sum";
+    return 0;
+}
